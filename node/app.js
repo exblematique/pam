caad895c20bc03f3f85735862703a6cb9e51ef4d
pam <pam@ssh1.paris1.alwaysdata.com>
2020-05-19 15:19:31 +0200

    Initial commit, just a hello world

---
+++ node/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/node', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, process.env.IP, function () {
+    console.log('Example app started!')
+})
