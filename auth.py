84770343e91ab1abec7f112a3a21a4e2d57256a3
esaip <esaip@VM1>
2020-05-19 11:55:50 +0200

    Send information from MQTT to Firebase woth UID fixed in the code

--- auth.py
+++ auth.py
@@ -2,23 +2,78 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-#Atuh with json
+import paho.mqtt.client as mqtt
+
+#To exit porperly the program
+from signal import signal, SIGINT
+
+continueProg = True
+def handler(sig, frame):
+    global continueProg
+    continueProg = False
+signal(SIGINT, handler)
+
+
+#Value to setup program
+mqtt_topic_receive = "sensors/"
+mqtt_broker_ip = "localhost"
+mqtt_port = 1883
+uid = "309pfB9bbU4dB9OAqvQt"
+
+##########   Connection to Firebase
+#Using authentification with json containt private key of server.
+#TODO Needed to secure this !
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
+db = firestore.client().collection('Users').document(uid).collection("data")
+
+##########   Connection to mqtt broker
+client = mqtt.Client()
+
+#This function is called when this program is conncted to the broker 
+def on_connect(client, userdata, flag, rc):
+    print("Connected!"+str(rc))
+    client.subscribe(mqtt_topic_receive+"#")
+
+#This function is called when the program receive a message from broker
+def on_message(client, userdata, msg):
+    topic = msg.topic.split('/')
+    name = topic[1]
+    for t in topic[2:]:
+        name += "-" + t 
+    #name = msg.topic[len(mqtt_topic_receive):]
+    data = {
+        u'name': name,
+        u'lastValue': float(msg.payload)
+    }
+    print("-----\nName: " + name + "\nData: " + str(msg.payload))
+    db.document(name).set(data)
+    
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(mqtt_broker_ip, mqtt_port)
+client.loop_start()
+
+while continueProg:
+    pass
 
 # Use the application default credentials
+#TODO Clean code
 """cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred, {
       'projectId': 'pam-iot',
     })
-"""
+""
 
-db = firestore.client()
 data = {
         u'name': u'Los Angeles',
         u'state': u'CA',
         u'country': u'USA'
     }
-
+"""
 # Add a new doc in collection 'cities' with ID 'LA'
-db.collection(u'cities').document(u'LA').set(data)
+#db.collection(u'cities').document(u'LA').set(data)
+
+client.disconnect()
+print("MQTT is disconnected")
