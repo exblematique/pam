7776646cf1522eaf9e32d6e1abc670f7e4bf9ec8
pam <pam@alwaysdata.com>
2020-05-19 17:02:05 +0200

    Auth with Firebase works

--- node/app.js
+++ node/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/node', function (req, res) {
+
+app.use('/node', express.static('www'));
+/*app.get('/node', function (req, res) {
     res.send('Hello World!')
 })
+*/
 
 app.listen(process.env.PORT, process.env.IP, function () {
     console.log('Example app started!')
